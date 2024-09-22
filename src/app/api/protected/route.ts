170c6dd4513cb361eb1f6d4f2b32c86da779f113
Absar Ahmed <absar8857@gmail.com>
2024-09-23 01:15:37 +0500

    protected route changed

--- src/app/api/protected/route.ts
+++ src/app/api/protected/route.ts
@@ -1,32 +1,40 @@
 import { NextRequest, NextResponse } from "next/server";
 import { registerUserServiceThroughGoogle } from "@/Service/googleUserService";
 import { connectToMongoDB } from "@/database/connection";
+import { JwtAuthService } from "@/app/utils/jwt-service";
 
 export async function POST(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
   const body = await req.json();
   const { userInfo } = body;
 
-  // If both tokens are missing, the user is not authenticated
-  // if (!token && !userInfo) {
-  //   return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
-  // }
-
-  if (!userInfo) {
+  // Check for token and user info
+  if (!token && !userInfo) {
     return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
   }
-  const {
-    id: googleId,
-    email,
-    verified_email,
-    name: fullName,
-    picture: profileImage,
-  } = userInfo;
 
-  try {
-    await connectToMongoDB();
-    // Handle Google OAuth login
-    if (userInfo) {
+  if (token) {
+    // Verify the JWT token
+    const jwtAuthService = new JwtAuthService();
+    const decodedToken = jwtAuthService.verifyToken(token);
+
+    if (!decodedToken) {
+      return NextResponse.json({ message: "Invalid token" }, { status: 401 });
+    }
+  }
+
+  if (userInfo) {
+    const {
+      id: googleId,
+      email,
+      verified_email,
+      name: fullName,
+      picture: profileImage,
+    } = userInfo;
+
+    try {
+      await connectToMongoDB();
+      // Handle Google OAuth login
       const googleUser = {
         googleId,
         email,
@@ -37,8 +45,11 @@ export async function POST(req: NextRequest) {
       const response = await registerUserServiceThroughGoogle(googleUser);
 
       return response;
+    } catch (err) {
+      return NextResponse.json(
+        { message: "Error processing user" },
+        { status: 500 }
+      );
     }
-  } catch (err) {
-    return NextResponse.json({ message: "Invalid token" }, { status: 401 });
   }
 }
